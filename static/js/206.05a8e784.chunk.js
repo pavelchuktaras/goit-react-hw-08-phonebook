f7fcf9ff3cb365c1cb12a33c0d4a7f867a1f7169
pavelchuktaras <121377064+pavelchuktaras@users.noreply.github.com>
2023-09-15 06:34:41 +0000

    Deploying to gh-pages from @ pavelchuktaras/goit-react-hw-08-phonebook@9d5a9984438b7632b129b9de8f1d8eab2919982b 🚀

---
+++ static/js/206.05a8e784.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[206],{1206:function(e,n,t){t.r(n),t.d(n,{default:function(){return c}});var o=t(4270),r=t(3329);function c(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)(o.q,{children:(0,r.jsx)("title",{children:"Phonebook"})})})}}}]);
+//# sourceMappingURL=206.05a8e784.chunk.js.map
